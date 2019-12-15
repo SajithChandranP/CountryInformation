45c02d4e9dac0d7725e946ff8760082dae295efd
Sajith <sajith@mobiiworld.com>
2019-12-15 12:03:03 +0530

    [Initial commit]

---
+++ CountryInformation/CountryInformation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryInformation
+//
+//  Created by Mobiiworld on 15/12/2019.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
